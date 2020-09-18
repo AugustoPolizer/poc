6f3572687a16198bbabe2d4d74b15e453c6a9b5a
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-18 01:49:26 -0300

    estruturação inicial do código

---
+++ poc/src/lib.rs
@@ -0,0 +1,25 @@
+mod lexer {
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn template() {
+            assert_eq!(1 + 1, 2);
+        }
+    }
+}
+
+mod parser {
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn template() {
+            assert_eq!(1 + 1, 2);
+        }
+    }
+}
