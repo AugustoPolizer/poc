68cd26551a62eff94681267d860f80766b5e38bb
AugustoPolizer <augustohp@dcc.ufmg.br>
2020-09-21 02:34:46 -0300

    implementação de diversos tokens

--- poc/src/lib.rs
+++ poc/src/lib.rs
@@ -1,71 +1,119 @@
 mod lexer {
 
     struct Lexer<'a> {
-        code_iterator: std::iter::Peekable< std::str::Chars<'a> >
-    } 
+        code_iterator: std::iter::Peekable<std::str::Chars<'a>>,
+    }
 
     enum TokenType {
         NAME,
-        EOF
+        LPARENTHESES,
+        RPARENTHESES,
+        LBRACK,
+        RBRACK,
+        LBRACE,
+        RBRACE,
+        SEMICOLON,
+        EOF,
+        UNKNOWN
     }
 
     struct Token {
-        token_type : TokenType,
+        token_type: TokenType,
         text: String,
     }
 
     impl Token {
-        fn new(token_type: TokenType, text: String) -> Token{
-            Token {
-                token_type,
-                text
-            }
+        fn new(token_type: TokenType, text: String) -> Token {
+            Token { token_type, text }
         }
     }
 
-
-    impl <'a> Lexer<'a>{ 
-        fn new(raw_code: & 'a str) -> Lexer<'a>{
+    impl<'a> Lexer<'a> {
+        fn new(raw_code: &'a str) -> Lexer<'a> {
             Lexer {
-                code_iterator: raw_code.chars().peekable()
+                code_iterator: raw_code.chars().peekable(),
             }
         }
 
         fn get_token(&mut self) -> Token {
-            
-            let c = match self.code_iterator.next() {
-                Some(c) => c,
+
+            let mut lookahead = match self.code_iterator.peek() {
+                Some(c) => *c,
                 None => return Token::new(TokenType::EOF, String::from("EOF"))
             };
-            
-            let result: Token = match c {
-                ' ' => consume_whitespace(),
+
+            // Remove whitespaces 
+            if lookahead == ' ' || lookahead == '\n' || lookahead == '\t' || lookahead == '\r' {
+                self.code_iterator.next();
+                loop {
+                    lookahead = match self.code_iterator.peek() {
+                        Some(c) => *c,
+                        None => return Token::new(TokenType::EOF, String::from("EOF")),
+                    };
+                    match lookahead {
+                        ' ' | '\n' | '\t' | '\r' => self.code_iterator.next(),
+                        _ => break,
+                    };
+                }
             }
+            println!("valor do lookahead: {}", lookahead);
+
+            // FIXME: Não retornar NAME para todas as cadeias que começam com caracteres
+            // TODO: Refator, extrair metodo
+            let result = match lookahead {
+                'a'..='z' | 'A'..='Z' => {
+                    let mut buffer = String::new();
+                    loop {
+                        buffer.push(lookahead);
+                        self.code_iterator.next();
+                        lookahead = match self.code_iterator.peek() {
+                            Some(c) => *c,
+                            None => break,
+                        };
+                        // FIXME: Incompleto, tem que checar mais coisas!!
+                        if !lookahead.is_ascii_alphabetic() && !(lookahead == '_') {
+                            break
+                        }
+                    }
+                    Token::new(TokenType::NAME, buffer)
+                }
+                ';' => {
+                    self.code_iterator.next();
+                    Token::new(TokenType::SEMICOLON, String::from(";")) 
+                }
+                '(' => {
+                    self.code_iterator.next();
+                    Token::new(TokenType::LPARENTHESES, String::from("(")) 
+                }
+                ')' => {
+                    self.code_iterator.next();
+                    Token::new(TokenType::RPARENTHESES, String::from(")")) 
+                }
+                '[' => {
+                    self.code_iterator.next();
+                    Token::new(TokenType::LBRACK, String::from("[")) 
+                }
+                ']' => {
+                    self.code_iterator.next();
+                    Token::new(TokenType::RBRACK, String::from("]")) 
+                }
+                '{' => {
+                    self.code_iterator.next();
+                    Token::new(TokenType::LBRACE, String::from("{")) 
+                }
+                '}' => {
+                    self.code_iterator.next();
+                    Token::new(TokenType::RBRACE, String::from("}")) 
+                }
+                _ => {
+                    self.code_iterator.next();
+                    Token::new(TokenType::UNKNOWN, String::from("UNK"))
+                }
+            };
 
             result
         }
-        
-        fn comsume(&mut self) -> Result<char, &'static str>{
-            let result = match self.code_iterator.next() {
-                Some(c) => Ok(c),
-                None => return Err("Comsume any empty stream")
-            };
 
-            return result
-        }
-
-        fn consume_whitespaces(&mut self) {
-            let mut lookahead;
-            loop { 
-                lookahead = match self.code_iterator.peek(){
-                    Some(c) => c,
-                    None => break
-                };
-                match &lookahead {
-                    ' ' | '\n' | '\t' | '\r' => self.code_iterator.next(),
-                } 
-            }
-        }
     }
 
     #[cfg(test)]
@@ -79,16 +127,17 @@ mod lexer {
                     return 1;
                 }
                 ";
-            let mut lex: Lexer = Lexer::new(& code);
+            let mut lex: Lexer = Lexer::new(&code);
             assert_eq!(lex.get_token().text, "function");
             assert_eq!(lex.get_token().text, "one");
             assert_eq!(lex.get_token().text, "(");
             assert_eq!(lex.get_token().text, ")");
-            assert_eq!(lex.get_token().text, "->");
+            assert_eq!(lex.get_token().text, "UNK");
+            assert_eq!(lex.get_token().text, "UNK");
             assert_eq!(lex.get_token().text, "int");
             assert_eq!(lex.get_token().text, "{");
             assert_eq!(lex.get_token().text, "return");
-            assert_eq!(lex.get_token().text, "1");
+            assert_eq!(lex.get_token().text, "UNK");
             assert_eq!(lex.get_token().text, ";");
             assert_eq!(lex.get_token().text, "}");
             assert_eq!(lex.get_token().text, "EOF");
--- poc/src/main.rs
+++ poc/src/main.rs
@@ -4,15 +4,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-       println!("Missing the filename");
-       std::process::exit(1);
+        println!("Missing the filename");
+        std::process::exit(1);
     }
 
     let file_name = &args[1];
 
-    let file_contents = fs::read_to_string(file_name)
-        .expect(& format!("Could not read file {}", file_name));
+    let file_contents =
+        fs::read_to_string(file_name).expect(&format!("Could not read file {}", file_name));
 
-    println!("{}", file_contents)    
+    println!("{}", file_contents)
 }
-
